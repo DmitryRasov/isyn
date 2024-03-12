3a5d356bea6c5ae822b18cd87f3e820a153043eb
DmitryRasov <letucepray@yandex.ru>
2024-03-12 17:38:12 +0300

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import {useCallback, useEffect, useState} from "react";
+import UserForm from "./components/user/UserForm";
+import UserPreviewList from "./components/user/previews/UserPreviewList";
+import {getUsers} from "./services/getUsers";
 
 function App() {
+    const [users, setUsers] = useState([])
+    const [activeUser, setActiveUser] = useState({})
+    const [page, setPage] = useState(1)
+    const [editId, setEditId] = useState(null)
+
+    useEffect(() => {
+        (async function () {
+            const newUsers = await getUsers(page)
+            setUsers([...users, ...newUsers])
+        })()
+    }, [page])
+
+    const handleUser = useCallback((user) => {
+        setActiveUser(user);
+    }, []);
+
+    const updateUsers = useCallback(() => {
+        setPage((prevPage) => prevPage + 1);
+    }, []);
+
+    const cancelEdit = useCallback((param) => {
+        setEditId(param);
+    }, []);
+
+    const handleEdit = useCallback((id) => {
+        setEditId(id);
+    }, []);
+
+    const handleSave = useCallback((updatedUser) => {
+        const updatedUsers = users.map((user) =>
+            user.id === updatedUser.id ? updatedUser : user
+        );
+        setUsers(updatedUsers);
+        setEditId(null);
+        setActiveUser(updatedUser);
+    }, [users]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <div className="wrapper">
+                <UserPreviewList
+                    users={users}
+                    handleUser={handleUser}
+                    updateUsers={updateUsers}
+                    cancelEdit={cancelEdit}
+
+                />
+                <UserForm
+                    user={activeUser}
+                    handleEdit={handleEdit}
+                    editId={editId}
+                    cancelEdit={cancelEdit}
+                    handleSave={handleSave}
+                />
+        </div>
   );
 }
 
