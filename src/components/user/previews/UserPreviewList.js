f1c753e0cc49647e820073b33648256aa7296762
DmitryRasov <letucepray@yandex.ru>
2024-03-14 16:21:02 +0300

    Добавил виртуализацию через индексы видимых компонентов списка

--- src/components/user/previews/UserPreviewList.js
+++ src/components/user/previews/UserPreviewList.js
@@ -1,31 +1,54 @@
-import React from 'react'
-import { FixedSizeList as ListItem } from 'react-window'
-import UserPreview from "./UserPreview"
+import React, { useRef, useEffect, useState } from 'react';
+import UserPreview from "./UserPreview";
 
 const UserPreviewList = ({ users, handleUser, updateUsers, cancelEdit }) => {
-    const Row = ({ index, style }) => {
-        const user = users[index]
-        return (
-            <div style={style}>
-                <UserPreview upd={upd} user={user} handleUser={handleUser} cancelEdit={cancelEdit}/>
-            </div>
-        );
-    };
-    console.log('we are in vanilla virt branch')
+    const containerRef = useRef(null)
+    const [startIndex, setStartIndex] = useState(0)
+    const [endIndex, setEndIndex] = useState(7)
+
+    useEffect(() => {
+        const updateVisibleIndices = () => {
+            if (!containerRef.current) return
+
+            const containerHeight = containerRef.current.clientHeight
+            const itemHeight = 50
+
+            const newStartIndex = Math.floor(containerRef.current.scrollTop / itemHeight)
+            const newEndIndex = Math.min(
+                users.length - 1,
+                Math.ceil((containerRef.current.scrollTop + containerHeight) / itemHeight)
+            )
+            setStartIndex(newStartIndex)
+            setEndIndex(newEndIndex)
+        }
+        containerRef.current.addEventListener('scroll', updateVisibleIndices)
+        updateVisibleIndices()
+
+        return () => {
+            containerRef.current.removeEventListener('scroll', updateVisibleIndices)
+        }
+    }, [users])
+
     const upd = (id) => {
         updateUsers()
         console.log(id)
     }
+
     return (
-        <ListItem
-            itemCount={users.length}
-            itemSize={50}
-            width={400}
-            height={400}
-        >
-            {Row}
-        </ListItem>
+        <div ref={containerRef} style={{ maxHeight: '400px', overflowY: 'auto', width: '400px'}}>
+            <div style={{ paddingTop: `${startIndex * 50}px`, paddingBottom: `${(users.length - endIndex - 1) * 50}px` }}>
+                {users.slice(startIndex, endIndex + 1).map((user) => (
+                    <UserPreview
+                        key={user.id}
+                        user={user}
+                        handleUser={handleUser}
+                        cancelEdit={cancelEdit}
+                        upd={upd}
+                    />
+                ))}
+            </div>
+        </div>
     );
 };
 
-export default UserPreviewList
+export default UserPreviewList;
